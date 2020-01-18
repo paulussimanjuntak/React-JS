62664d909c54667b9ced82399cd684d9a31d6896
Paulus Simanjuntak <paulus@Pauluss-MacBook-Pro.local>
2020-01-18 12:46:43 +0800

    css modules

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
 
   render() {
     let persons = null
-    let btnClass = [classes.Button]
+    let btnClass = ''
 
     if (this.state.showPersons) {
       persons = (
@@ -61,7 +61,7 @@ class App extends Component {
           })}
         </div>
       );
-      btnClass.push(classes.Red)
+      btnClass = classes.Red
     }
 
     const assignedClasses = []
@@ -76,7 +76,7 @@ class App extends Component {
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button className={btnClass.join(' ')} 
+        <button className={btnClass} 
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}       
       </div>
