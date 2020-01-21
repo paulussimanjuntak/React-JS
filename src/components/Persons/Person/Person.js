b033e4e5c3ecd58902847026952bb0826c8ba4a6
Paulus Simanjuntak <paulus@Pauluss-MacBook-Pro.local>
2020-01-21 20:51:50 +0800

    Passing Unknown Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Aux from '../../../hoc/Aux'
-import WithClass from '../../../hoc/WithClass'
+import withClass from '../../../hoc/withClass'
 import classes from './Person.css'
 
 class Person extends Component {
@@ -8,14 +8,12 @@ class Person extends Component {
         console.log('[Person.js] rendering...')
         return (
             <Aux>
-                <WithClass className={classes.Person}>
                     <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                     <p>{this.props.children}</p>
                     <input type="text" onChange={this.props.changed} value={this.props.name} />
-                </WithClass>                
             </Aux>
         )
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
