2cd1585039c4eed11ad625664760d95ab4fd2efb
Paulus Simanjuntak <paulus@Pauluss-MacBook-Pro.local>
2020-01-22 11:13:14 +0800

    Context API

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import classes from './Cockpit.css'
+import AuthContext from '../../context/auth-context'
 
 const cockpit = (props) => {
     const toggleBtnRef = useRef(null)
@@ -46,10 +47,15 @@ const cockpit = (props) => {
                 className={btnClass} 
                 onClick={props.clicked}>Toggle Persons
             </button>
-            <button
-                onClick={props.login} >
-                Log in
-            </button>
+            <AuthContext.Consumer>
+                {(context) => 
+                    <button
+                        onClick={context.login} >
+                        Log in
+                    </button>
+                }   
+            </AuthContext.Consumer>
+            
         </div>
     )
 }
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Aux from '../../../hoc/Aux'
 import withClass from '../../../hoc/withClass'
 import classes from './Person.css'
+import AuthContext from '../../../context/auth-context'
 
 class Person extends Component {
     constructor(props){
@@ -19,7 +20,10 @@ class Person extends Component {
         console.log('[Person.js] rendering...')
         return (
             <Aux>
-                {this.props.isAuth ? <p>Authenticated!!</p> : <p>Please log in</p>}
+                <AuthContext.Consumer>
+                    {(context) => context.authenticated ? <p>Authenticated!!</p> : <p>Please log in</p>}
+                </AuthContext.Consumer>
+        
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
